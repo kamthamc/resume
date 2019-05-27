b638ce13620958a066048c2b7b226ca12729fd8f
Chaitanya K <kamthamc@users.noreply.github.com>
2019-05-26 22:58:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './App';
 import './Styles/Fonts.css';
